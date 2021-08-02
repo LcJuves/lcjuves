fa9ada0aefecec4612d084d1fcf26c3046d8a34f
Liangcheng Juves <liangchengj@outlook.com>
2021-08-03 00:51:19 +0800

    Brand new JavaScript

---
+++ java/android/gen_obfuscation_dict.js
@@ -0,0 +1,51 @@
+/**
+ * Created at 2021/8/1 23:15
+ *
+ * @author Liangcheng Juves
+ */
+
+// @ts-check
+
+// deno run --allow-write gen_obfuscation_dict.js
+// node gen_obfuscation_dict.js
+
+const result = new Set();
+
+const keys = ["l", "I", "1", "O", "o", "0", "$"];
+
+for (let o = 1; o < 100000; o++) {
+  for (let len = 6; len < 13; len++) {
+    let tmp = keys[Math.floor(Math.random() + 0.5)];
+    for (let i = 1; i < len + 1; i++) {
+      tmp += keys[Math.floor(Math.random() * keys.length)];
+    }
+    result.add(tmp);
+  }
+}
+
+const runOnDeno = () => {
+  return typeof Deno === "object";
+};
+
+const outputFileName = "obfuscation.dict";
+
+if (runOnDeno()) {
+  const encoder = new TextEncoder();
+  const file = Deno.openSync(outputFileName, { write: true, create: true });
+
+  for (const item of result) {
+    const data = encoder.encode(`${item}\n`);
+    Deno.writeSync(file.rid, data);
+  }
+
+  Deno.close(file.rid);
+} else {
+  const fs = await import("fs");
+  const writeStream = fs.createWriteStream(outputFileName);
+  for (const item of result) {
+    writeStream.write(`${item}\n`);
+  }
+  writeStream.end();
+}
+
+console.log(`Generate "${outputFileName}" successfully!`);
