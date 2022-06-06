b7b8d7e98802d2cac4b76904f8a35ff97af9c4d8
Liangcheng Juves <liangchengj@outlook.com>
2022-06-06 17:39:29 +0800

    Update config_my_git.sh

--- clang/linux-like/config_my_git.sh
+++ clang/linux-like/config_my_git.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2129
 
 USER_NAME="Liangcheng Juves"
 USER_EMAIL="liangchengj@outlook.com"
@@ -17,6 +18,9 @@ ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa -q -b 4096 \
 ssh-keygen -t ed25519 -N '' -f ~/.ssh/id_ed25519 -q -b 4096 \
     -C "$USER_EMAIL"
 
+cat ~/.ssh/id_rsa.pub >>~/.ssh/authorized_keys
+cat ~/.ssh/id_ed25519.pub >>~/.ssh/authorized_keys
+
 ssh-keygen -t dsa -N '' -f /etc/ssh/ssh_host_dsa_key -q -C "$USER_EMAIL" &&
     ssh-keygen -t rsa -N '' -f /etc/ssh/ssh_host_rsa_key -q -b 4096 -C "$USER_EMAIL" &&
     ssh-keygen -t ecdsa -N '' -f /etc/ssh/ssh_host_ecdsa_key -q -C "$USER_EMAIL" &&
@@ -28,3 +32,9 @@ ssh-keygen -r bitbucket.org
 ssh-keygen -r jihulab.com
 ssh-keygen -r gitee.com
 ssh-keygen -r localhost
+
+ssh-keyscan -t rsa github.com >>~/.ssh/known_hosts
+ssh-keyscan -t rsa gitlab.com >>~/.ssh/known_hosts
+ssh-keyscan -t rsa bitbucket.org >>~/.ssh/known_hosts
+ssh-keyscan -t rsa jihulab.com >>~/.ssh/known_hosts
+ssh-keyscan -t rsa gitee.com >>~/.ssh/known_hosts
